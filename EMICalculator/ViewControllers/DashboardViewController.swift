3274f2dd74d948c36bcfb2390bf671636f7c3287
Karamjit Singh <karamjitsingh1989@gmail.com>
2022-07-22 17:12:56 +0530

    SPM added

---
+++ EMICalculator/ViewControllers/DashboardViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DashboardViewController.swift
+//  EMICalculator
+//
+//  Created by Harsimrat on 22/07/22.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
